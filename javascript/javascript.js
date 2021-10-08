2119927a028e3e37968054113afe3b63a0d16e7e
Florencia Barbero <60927375+mssRobot@users.noreply.github.com>
2021-10-08 18:38:09 -0300

    add

---
+++ javascript/javascript.js
@@ -0,0 +1,20 @@
+
+document.getElementById('convert').addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+
+    var distance = parseFloat(document.getElementById('distance').value);
+    
+    if (distance) {
+        var conversion = (distance*1.609344).toFixed(3);
+        
+        var answer =document.getElementById('answer');
+
+
+        answer.innerHTML = `<h2> ${distance} miles converts to ${conversion} kilometers</h2>`;
+    } 
+        else{
+            answer.innerHTML = `<h2> please provide the number</h2>`;
+
+        }
+});
